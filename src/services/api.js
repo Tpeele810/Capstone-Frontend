37ec84fd49079553d710cbbd8cc3910cdd4d8966
tpeele810 <tiffany.peele1@gmail.com>
2025-05-14 10:00:53 -0400

    added comma

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/',
-  baseURL: 'https://capstone-backend-1-3fr5.onrender.com/application'
+  baseURL: 'https://capstone-backend-1-3fr5.onrender.com/application',
   headers: {'Content-Type': 'application/json'} 
 });
 
